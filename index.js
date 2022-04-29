28afa934d1d1dd6f4dc1d53deacca1c861d9d15c
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-04-29 09:46:14 +0200

    Update: Moved some stuff in index.js

--- index.js
+++ index.js
@@ -5,36 +5,13 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
 
 // const DiscordSnowgiving = require('./notifiers/discord-snowgiving');
 const ShopAnthonyWang = require('./notifiers/shopanthonywang')
 const Keychron = require('./notifiers/keychron')
 const ZUnivers = require('./notifiers/zunivers')
 // const ZLan = require('./notifiers/zlan');
-
-client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`.yellow)
-
-    //new DiscordSnowgiving(client);
-    new ShopAnthonyWang(client)
-    new Keychron(client)
-    new ZUnivers(client)
-    // new ZLan(client);
-
-    try {
-        console.log('Started refreshing application (/) commands.'.gray)
-
-        await rest.put(Routes.applicationCommands(process.env.DISCORD_APP_ID), {
-            body: commands,
-        })
-
-        console.log('Successfully reloaded application (/) commands.'.green)
-    } catch (error) {
-        console.error(error)
-    }
-})
-
-client.login(process.env.DISCORD_TOKEN).then()
 
 const commands = [
     {
@@ -70,4 +47,26 @@ const commands = [
     },
 ]
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`.yellow)
+
+    //new DiscordSnowgiving(client);
+    new ShopAnthonyWang(client)
+    new Keychron(client)
+    new ZUnivers(client)
+    // new ZLan(client);
+
+    try {
+        console.log('Started refreshing application (/) commands.'.gray)
+
+        await rest.put(Routes.applicationCommands(process.env.DISCORD_APP_ID), {
+            body: commands,
+        })
+
+        console.log('Successfully reloaded application (/) commands.'.green)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+client.login(process.env.DISCORD_TOKEN).then()
