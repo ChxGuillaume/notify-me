e23553c08823fb48dfc7293110fe49d50db23ca0
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-06-10 15:16:28 +0200

    Update: New Releases AW

--- notifiers/shopanthonywang.js
+++ notifiers/shopanthonywang.js
@@ -1,106 +1,124 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const cron = require('node-cron');
-const logger = require('../utils/logger');
-const {sendMessage} = require('../utils/messages');
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const cron = require('node-cron')
+const logger = require('../utils/logger')
+const { sendMessage, deleteMessage } = require('../utils/messages')
 
 module.exports = class ShopAnthonyWang {
-
     constructor(client) {
-        this.client = client;
-        this.file_name = 'ShopAnthonyWang.json';
+        this.client = client
+        this.file_name = 'ShopAnthonyWang.json'
 
-        this.shoes = {};
+        this.shoes = {}
         if (fs.existsSync(path.join('data', this.file_name))) {
-            const file = fs.readFileSync(path.join('data', this.file_name), {encoding: 'utf8'});
-            this.shoes = JSON.parse(file) || {};
+            const file = fs.readFileSync(path.join('data', this.file_name), {
+                encoding: 'utf8',
+            })
+            this.shoes = JSON.parse(file) || {}
         }
 
-        cron.schedule('*/5 * * * *', () => {
-            this.fetch();
-        }, {});
-        this.fetch();
+        cron.schedule(
+            '*/5 * * * *',
+            () => {
+                this.fetch()
+            },
+            {}
+        )
+        this.fetch()
+
+        client.on('interactionCreate', async (interaction) => {
+            if (!interaction.isButton()) return
+
+            if (interaction.customId.startsWith('aw-new-shoes')) {
+                await deleteMessage(this.channel(), interaction.message.id)
+
+                interaction.reply({
+                    content: 'Message Deleted!',
+                    ephemeral: true,
+                })
+            }
+        })
     }
 
     save() {
-        fs.writeFileSync(path.join('data', this.file_name), JSON.stringify(this.shoes));
+        fs.writeFileSync(
+            path.join('data', this.file_name),
+            JSON.stringify(this.shoes)
+        )
     }
 
     channel() {
-        return this.client
-            .guilds.cache.find(guild => guild.id === '914899103035564132')
-            .channels.cache.find(channel => channel.id === '914899158375215105')
+        return this.client.guilds.cache
+            .find((guild) => guild.id === '914899103035564132')
+            .channels.cache.find(
+                (channel) => channel.id === '914899158375215105'
+            )
     }
 
     fetch() {
         axios
-            .get('https://shopanthonywang.com/a/search/all?filter_shop_by_size%5B0%5D=US%2011&filter_shop_by_size%5B1%5D=US%2012', {
-                headers: {
-                    cookie: 'cart_currency=EUR;'
+            .get('https://shopanthonywang.com/products.json?limit=500&page=1')
+            .then(({ data: { products } }) => {
+                if (!this.shoes.newProducts) {
+                    this.shoes.newProducts = {}
+
+                    products.forEach((product) => this.addNewProduct(product))
+
+                    this.save()
+                } else {
+                    products.forEach((product) => {
+                        if (
+                            !this.shoes.newProducts[product.id] ||
+                            product.id === 7449345229015
+                        ) {
+                            const newProduct = this.addNewProduct(product)
+
+                            sendMessage(
+                                this.channel(),
+                                '921116258110406717',
+                                {
+                                    title: product.title,
+                                    description: product.description,
+                                    url: `https://shopanthonywang.com/products/${product.handle}`,
+                                    thumbnail: product.images[0].src,
+                                    price: `$${product.variants[0].price}`,
+                                    options: [newProduct.description],
+                                    deleteButton: true,
+                                },
+                                [
+                                    {
+                                        name: 'Sizes',
+                                        value: product.variants
+                                            .filter((v) => v.available)
+                                            .map((v) => v.title)
+                                            .join(' - '),
+                                    },
+                                ],
+                                `aw-new-shoes-${product.id}`
+                            ).then()
+                        }
+                    })
+
+                    this.save()
                 }
-            })
-            .then(async (response) => {
-                const $shoesPage = cheerio.load(response.data);
-                const newShoes = {};
-
-                for (const item of $shoesPage('#shopify-section-collection-template').find('.grid__item')) {
-                    if ($shoesPage(item).text().trim() === 'Sorry, there are no products in this collection') continue;
-
-                    const url = 'https://shopanthonywang.com' + $shoesPage(item).find('.grid-view-item__link').attr('href');
-
-                    if (!this.shoes[url]) {
-                        await axios.get(url)
-                            .then(async ({data}) => {
-                                const $shoePage = cheerio.load(data);
-
-                                let {
-                                    title,
-                                    description,
-                                    featured_image: image,
-                                    price,
-                                    variants,
-                                } = JSON.parse($shoePage('script').toArray().find(e => $shoesPage(e).attr('id') === 'ProductJson-product-template').children[0].data);
-
-                                description = $shoePage(description)
-                                    .text()
-                                    .replace('CHECK YOUR SIZE', '')
-                                    .trim()
-                                    .split('\n')
-                                    .filter(el => el && el.trim());
-
-                                image = `https:${image}`;
-                                price = `${price / 100}€`;
-
-                                variants = variants
-                                    .filter(({available}) => available)
-                                    .map(({title}) => (`${title}`));
-
-                                await sendMessage(
-                                    this.channel(),
-                                    '921116258110406717',
-                                    {title, url, image, price, options: description},
-                                    [{
-                                        name: 'Available Sizes',
-                                        value: variants.join(' - ')
-                                    }],
-                                    'shop-anthony-wang-' + url
-                                );
-
-                                newShoes[url] = true;
-                            });
-                    } else {
-                        newShoes[url] = true;
-                    }
-                }
-
-                this.shoes = newShoes;
-
-                this.save();
-
-                logger('ShopAnthonyWang Checked!');
             })
     }
 
+    addNewProduct(product) {
+        return (this.shoes.newProducts[product.id] = {
+            id: product.id,
+            title: product.title,
+            handle: product.handle,
+            description: product.body_html
+                .replace(/<[^>]*>|CHECK YOUR SIZE/g, '')
+                .trim(),
+            images: product.images,
+            price: product.variants[0].price,
+            variants: product.variants,
+            created_at: product.created_at,
+            updated_at: product.updated_at,
+        })
+    }
 }
